8589ffd6a52b4e496d7c737e0d4461ec17ecd9e7
LoganPletcher <lpletch95@hotmail.com>
2016-01-19 17:57:42 -0600

    Unity Commit 1

---
+++ Ball Grid/Assets/Grid_Creation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid_Creation : MonoBehaviour {
+	public int n;
+
+	// Use this for initialization
+	void Start () {
+		float X = -1-((.5F*n)*2);
+		float Y = -1-((.5F*n)*2);
+		for (int i = 2; i <= n*2; i+=2)
+		{
+			for (int j = 2; j <= n*2; j+=2)
+			{
+				GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+				sphere.transform.position = new Vector3((X+j), (i+Y), 0);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Ball Grid/Assets/USP.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unity_Sphere_Grid : MonoBehaviour
+{
+    void Start ()
+    {
+		int X = 10;
+		int Y = 10;
+		for (int i = 1; i <= 5; i++)
+		{
+			for (int j = 1; i <= 5; i++)
+			{
+				GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+				sphere.transform.position = new Vector3((j*X), (i*Y), 10);
+			}
+		}
+
+    }
+
+    void Update ()
+    {
+
+    }
+}
