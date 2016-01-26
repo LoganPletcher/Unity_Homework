2a957191c9ee2349484d56c20e08b35297bab0ea
LoganPletcher <lpletch95@hotmail.com>
2016-01-26 17:53:10 -0600

    Unity Commit 4

---
+++ ADGP 130/Assets/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    bool pressW = false;
+    bool pressS = false;
+    bool pressA = false;
+    bool pressD = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 Previous = gameObject.transform.position;
+        //gameObject += .5f;
+        //gameObject.transform.position = new Vector3();
+        //print(gameObject.transform.position);
+        if (Input.GetKeyDown("w"))
+            pressW = true;
+        if (Input.GetKeyUp("w"))
+            pressW = false;
+        if (pressW == true)
+            gameObject.transform.position = new Vector3
+                (Previous.x, Previous.y, (Previous.z + .05f));
+        Previous = gameObject.transform.position;
+        if (Input.GetKeyDown("s"))
+            pressS = true;
+        if (Input.GetKeyUp("s"))
+            pressS = false;
+        if (pressS == true)
+            gameObject.transform.position = new Vector3
+                (Previous.x, Previous.y, (Previous.z - .05f));
+        Previous = gameObject.transform.position;
+        if (Input.GetKeyDown("a"))
+            pressA = true;
+        if (Input.GetKeyUp("a"))
+            pressA = false;
+        if (pressA == true)
+            gameObject.transform.position = new Vector3
+                ((Previous.x - .05f), Previous.y, Previous.z);
+        Previous = gameObject.transform.position;
+        if (Input.GetKeyDown("d"))
+            pressD = true;
+        if (Input.GetKeyUp("d"))
+            pressD = false;
+        if (pressD == true)
+            gameObject.transform.position = new Vector3
+                ((Previous.x + .05f), Previous.y, Previous.z);
+        Previous = gameObject.transform.position;
+        //print("W key was pressed");
+
+        if (Input.GetKeyDown("space"))
+            print("space key was pressed");
+    }
+
+
+}
